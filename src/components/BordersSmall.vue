6c45b16d5aff45d6601de8c42cd07e346bf1ef3b
nemanjamalesija <malesija13@gmail.com>
2023-07-06 23:52:21 +0200

    Add README

--- src/components/BordersSmall.vue
+++ src/components/BordersSmall.vue
@@ -16,51 +16,53 @@ import image14 from '../images/14.jpg'
 import image15 from '../images/15.jpg'
 </script>
 <template>
-  <div class="gallery-wrapper">
-    <figure class="gallery__item gallery__item--1">
-      <img :src="image1" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--2">
-      <img :src="image2" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--3">
-      <img :src="image3" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--4">
-      <img :src="image4" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--5">
-      <img :src="image5" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--6">
-      <img :src="image6" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--7">
-      <img :src="image7" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--8">
-      <img :src="image8" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--9">
-      <img :src="image9" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--10">
-      <img :src="image10" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--11">
-      <img :src="image11" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--12">
-      <img :src="image12" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--13">
-      <img :src="image13" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--14">
-      <img :src="image14" alt="image 1" />
-    </figure>
-    <figure class="gallery__item gallery__item--15">
-      <img :src="image15" alt="image 1" />
-    </figure>
-  </div>
+  <section>
+    <div class="gallery-wrapper">
+      <figure class="gallery__item gallery__item--1">
+        <img :src="image1" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--2">
+        <img :src="image2" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--3">
+        <img :src="image3" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--4">
+        <img :src="image4" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--5">
+        <img :src="image5" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--6">
+        <img :src="image6" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--7">
+        <img :src="image7" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--8">
+        <img :src="image8" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--9">
+        <img :src="image9" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--10">
+        <img :src="image10" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--11">
+        <img :src="image11" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--12">
+        <img :src="image12" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--13">
+        <img :src="image13" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--14">
+        <img :src="image14" alt="image 1" />
+      </figure>
+      <figure class="gallery__item gallery__item--15">
+        <img :src="image15" alt="image 1" />
+      </figure>
+    </div>
+  </section>
 </template>

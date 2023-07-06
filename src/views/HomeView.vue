3d380ed7919df70fa907201e58fb592c49c3eaaa
nemanjamalesija <malesija13@gmail.com>
2023-07-06 23:24:11 +0200

    Add no border gallery

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import BordersSmall from '../components/BordersSmall.vue'
+import NoBorder from '../components/NoBorder.vue'
 </script>
 
 <template>
   <main>
     <BordersSmall />
+    <NoBorder />
   </main>
 </template>
